8d6594341b713cc36f08966278f04c533e300add
Jonas Kinnvall <jonaskinnvall@gmail.com>
2018-11-15 09:53:11 +0100

    Correct projcet structure

--- Main.m
+++ Main.m
@@ -1,3 +1,9 @@
 %ABOB OMR project, Gabriel Berthold & Jonas Kinnvall
+clc;
+clear all;
 
-image1 = imread("images_training/le_1_example.jpg");
+%Read image
+image1 = imread('images_training/le_1_example.jpg');
+
+%Calls function with image1 as only argumentto return string of notes
+strout = tnm034(image1);
