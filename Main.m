e4923942c94c31a34bc7b81494aa9bc996cff4cd
Gabriel Berthold <gabbe631rstudent.liu.se>
2018-11-19 11:26:28 +0100

    remove staffs

--- Main.m
+++ Main.m
@@ -5,6 +5,7 @@ close all;
 
 %Read image
 image1 = imread('images_training/le_1_example.jpg');
-
+figure;
+imshow(image1);
 %Calls function with image1 as only argumentto return string of notes
 strout = tnm034(image1);
--- tnm034.m
+++ tnm034.m
@@ -10,32 +10,37 @@ strout = 1;
 % Makes image grayscale then double
 image = im2double(im);
 %Binarize image and calculate peak values of binarized image
-binIm = imbinarize(image(:,:,1), 0.8);
-binImswap = 1- binIm;
-h = 1- mean(binIm,2);
+binIm = 1- imbinarize(image(:,:,1), 0.6);
+
+h = mean(binIm,2);
 
 %Create threshhold value to filter peaks
 scan_peak_thresh = mean(h)+ 2* std(h,1);
 scan_filtered = (h>scan_peak_thresh);
 
-[pks, locs] = findpeaks(double(scan_filtered))
+[pks, locs] = findpeaks(double(scan_filtered));
 
 
+%figure;
+%imshow(binIm)
+%figure;
 
+%plot(h, 1:size(h))
+%figure(); imshow(binIm);
+%hold on; 
+%for i=1:size(locs,1)
+%    plot([1;size(image,2)],[locs(i,1);locs(i,1)],'r');
+%end
+%hold off;
 
-
-
-
-figure;
-imshow(binIm)
-figure;
-plot(h, 1:size(h))
-figure(); imshow(binImswap);
-hold on; 
-for i=1:size(locs,1)
-    plot([1;size(image,2)],[locs(i,1);locs(i,1)],'r');
+%Scan binaryImage and remove staffs
+for i=1:size(scan_filtered,1)
+    
+    if(scan_filtered(i,1) == 1)
+    binIm(i,:,:)=0;
+    end
 end
-hold off;
-
+figure;
+imshow(binIm);
 end
 
