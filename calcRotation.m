a417782721f4e5e9f2580545c3862eeb47573d95
Jonas Kinnvall <jonaskinnvall@gmail.com>
2018-12-10 10:13:01 +0100

    Rotation working on scanned

--- calcRotation.m
+++ calcRotation.m
@@ -1,8 +1,22 @@
-function [angle] = calcRotation(binIm)
+function [staffAngle] = calcRotation(rotIm)
 %Function to calculate rotation of image
 
+[H, theta, rho] = hough(rotIm);
 
+peak = houghpeaks(H);
 
+peak
 
+peak(2)
+
+angle = theta(peak(2));
+
+if(angle < 0)
+    staffAngle = 90 + theta(peak(2));
+elseif(angle >0)
+    staffAngle = 90 - theta(peak(2));
+end
+
+disp("Rotation angle: " + staffAngle);
 end
 
